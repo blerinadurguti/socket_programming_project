85f03d7d64980ddabff5e4c3d07e49ea0786d5d7
blinerakadriu <93934870+blinerakadriu@users.noreply.github.com>
2022-04-11 19:04:30 +0300

    Update server.js

--- server.js
+++ server.js
@@ -3,3 +3,12 @@ const net=require('net');
 
 const server=net.createServer()
 
+server.on('connection', (socket)=>{
+var remoteAddress=socket.remoteAddress + ":"+socket.remotePort;
+  console.log("New client connection is made %s", remoteAddress);
+
+    socket.on('data', (data)=>{
+        console.log("\nMessage received from %s: %s", remoteAddress, data)
+        socket.write(`Hello ${data} this is your file ${"C:\Users\PC\Desktop\tcp\server\text.html"}`)
+    })
+    
