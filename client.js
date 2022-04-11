dc7f1ed3d833530452f219d383b85a5f6f327041
Blerina2 <blerina.beka@student.uni-pr.edu>
2022-04-11 22:18:02 +0200

    Perfundimin e projektit

--- client.js
+++ client.js
@@ -1,6 +1,7 @@
 const net=require('net');
 const { send } = require('process');
 const readline=require('readline-sync')
+const fs = require("fs");
 
 const options={
     port:3000,
@@ -10,11 +11,15 @@ const options={
 const admin="Grupi";
 const password="11";
 
-
 const client=net.createConnection(options)
 
 client.on('connect', ()=>{
     console.log('U lidh me sukses!')
+    client.write('client:Pershendetje nga Blerina Beka\n');
+    client.write('client:Pershendetje nga Blerina Durguti\n');
+    client.write('client:Pershendetje nga Blerta Ujkani\n');
+    client.write('client:Pershendetje nga Blinera Kadriu\n');
+    client.write('client:Pershendetje nga te gjith\n');
     sendLine()
 })
 
@@ -24,19 +29,21 @@ client.on('data', (data)=>{
 })
 
 function sendLine(){
-    
-    var line1=readline.question("\nEmri juaj\t")
-    var line2=readline.question("Password-i\t")
+
+    let buffer;
+    let path;
+    const line1 = readline.question("\nEmri juaj\t");
+    const line2 = readline.question("Password-i\t");
 
     if(line1===admin&&line2===password){
         console.log("\n[1] Lexo, [2] Shkruaj, [3] Ekzekuto")
-        var type=readline.question("\nZgjedh\t")
-        var line=readline.question("\nShkruaj dicka...\t")
-    client.write(line)
+        const type = readline.question("\nZgjedh\t");
+        const line = readline.question("\nShkruaj dicka...\t");
+        client.write(line)
       switch(type) {
         case '1' :
-            var path = process.cwd();
-            var buffer = fs.readFileSync("C:/Users/PC/Documents/GitHub/socket_programming_project/server/text.txt");
+            path = process.cwd();
+            buffer = fs.readFileSync("text.txt");
             console.log(buffer.toString());
             client.end()
           break;
@@ -45,7 +52,7 @@ function sendLine(){
             fs.appendFile('text.txt', content, err => {
             if (err) {
               console.error(err)
-              return
+              return;
               }
              })
             break;
@@ -57,9 +64,9 @@ function sendLine(){
            console.log("ERROR! Shkruaj njerin nga opsionet: 1, 2 ose 3!!");
 }
     }
-    else{ 
-        var path = process.cwd();
-        var buffer = fs.readFileSync("C:/Users/PC/Documents/GitHub/socket_programming_project/server/text.txt");
+    else{
+        path = process.cwd();
+        buffer = fs.readFileSync("text.txt");
         console.log(buffer.toString());
         client.end()
     }   
--- server.js
+++ server.js
@@ -4,14 +4,32 @@ const net=require('net');
 const server=net.createServer()
 
 server.on('connection', (socket)=>{
-var remoteAddress=socket.remoteAddress + ":"+socket.remotePort;
-  console.log("Nje lidhje e re u krijua %s", remoteAddress);
+    const remoteAddress = socket.remoteAddress + ":" + socket.remotePort;
+    console.log("Nje lidhje e re u krijua %s", remoteAddress);
+
+    socket.on('CH01', function (from, msg) {
+        consoleLogDms(from, msg);
+    });
+
+    socket.on('CH02', function (from, msg) {
+        consoleLogDms(from, msg);
+    });
+
+    socket.on('CH03', function (from, msg) {
+        consoleLogDms(from, msg);
+    });
+
+    socket.on('CH04', function (from, msg) {
+        consoleLogDms(from, msg);
+    });
+
 
     socket.on('data', (data)=>{
         console.log("\nMesazhi u pranua nga %s: %s", remoteAddress, data)
-        socket.write('Pershendetje ${data} ky eshte file i juaj ${"C:/Users/PC/Documents/GitHub/socket_programming_project/html/landingPage.html"}')
+        socket.write('Pershendetje ${data} ky eshte file i juaj ${"C:/Users/Admin/WebstormProjects/socket_programming_project/landingPage.html"}')
+
     })
-socket.on('close',()=>{
+    socket.on('close',()=>{
         console.log("Komunikimi perfundoi")
     })
 
