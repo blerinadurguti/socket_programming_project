9d14fecb796bcd0c8e0a7fee4e1fa92c4b1fef3e
blinerakadriu <93934870+blinerakadriu@users.noreply.github.com>
2022-04-11 17:46:05 +0300

    Add files via upload

---
+++ client.js
@@ -0,0 +1,13 @@
+const net=require('net');
+const { send } = require('process');
+const readline=require('readline-sync')
+
+const options={
+    port:3000,
+    host: '127.0.0.1'
+}
+
+const admin="Grupi";
+const password="11";
+
+
